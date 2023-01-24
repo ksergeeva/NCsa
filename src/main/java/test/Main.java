779f61793084114b8c8532b28f2e280a528fd2e4
ksergeeva <111127945+ksergeeva@users.noreply.github.com>
2023-01-24 20:15:03 +0300

    adapter

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+        Person mom = new PersonBuilder()
+                .setName("Анна")
+                .setSurname("Вольф")
+                .setAge(31)
+                .setAddress("Сидней")
+                .build();
+        Person son = mom.newChildBuilder()
+                .setName("Антошка")
+               // .setAge(12)
+                .build();
+        System.out.println("У " + mom  + " есть сын, " + son );
+
+        try {
+
+            new PersonBuilder().build();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/test/PersonBuilder.java
@@ -0,0 +1,39 @@
+package test;
+
+public class PersonBuilder {
+    public String name;
+    public String surname;
+    public int age;
+    public String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Не допустимый возраст");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Отсутствует значение");
+        }
+
+        return new Person(name, surname, age, address);
+    }
+}
